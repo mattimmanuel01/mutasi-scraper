b43e464a25cb5278ba32d8c5a42a699bc5becfe6
Matthew Immanuel <matthew.immanuel@xero.com>
2023-05-23 23:50:07 +1000

    push working matt

--- examples/example.js
+++ examples/example.js
@@ -1,33 +1,64 @@
-// promisifying readline
-const readline = require('readline');
-const {ScrapBCA} = require("../");
-const { promisify } = require('util');
-const rl = readline.createInterface({
+const fs = require("fs");
+const path = require("path");
+const { ScrapBCA } = require("../");
+const { promisify } = require("util");
+
+const rl = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
+
 rl.question[promisify.custom] = (question) => {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     rl.question(question, resolve);
   });
 };
+
 const questionAsync = promisify(rl.question).bind(rl);
 
-(async () => {
+const runScrapper = async () => {
   const username = await questionAsync("BCA Username: ");
   const password = await questionAsync("BCA Password: ");
-  const tglawal = await questionAsync("Tanggal awal: ");
-  const blnawal = await questionAsync("Bulan awal: ");
-  const tglakhir = await questionAsync("Tanggal akhir: ");
-  const blnakhir = await questionAsync("Bulan akhir: ");
+  const today = new Date();
+  const month = String(today.getMonth() + 1);
+  const day = String(today.getDate());
 
-  const scraper = new ScrapBCA(username, password , {
-    headless : false
+  const scraper = new ScrapBCA(username, password, {
+    headless: false,
   });
 
-  var result = await scraper.getSettlement(tglawal, blnawal, tglakhir, blnakhir);
-  console.log(result);
+  // tglawal, blnawal, tglakhir, blnakhir
+  const result = await scraper.getSettlement(day, month, day, month);
 
-  // close readline interface
-  rl.close();
-})();
+  return {
+    currentTime: getCurrentTime(),
+    result,
+  };
+};
+
+const getCurrentTime = () => {
+  const today = new Date();
+  const hours = String(today.getHours()).padStart(2, "0");
+  const minutes = String(today.getMinutes()).padStart(2, "0");
+  return `Run on ${hours}:${minutes}`;
+};
+
+const appendToFile = (filePath, data) => {
+  fs.appendFileSync(filePath, data + "\n");
+};
+
+const intervalMinutes = 5;
+const resultFilePath = path.join(__dirname, "result.txt");
+
+const runScrapperAndAppend = async () => {
+  try {
+    const { currentTime, result } = await runScrapper();
+    const dataToAppend = `${currentTime}\n${JSON.stringify(result)}\n---\n`;
+    appendToFile(resultFilePath, dataToAppend);
+  } catch (error) {
+    console.error("Error running scrapper:", error);
+  }
+};
+
+runScrapperAndAppend();
+setInterval(runScrapperAndAppend, intervalMinutes * 60 * 1000);
